8348df3d84c3a23cd5b6c50ba0806d796cb0d613
kimyo <kimyo@ACER-NITRO5-AN5>
2022-08-03 23:04:42 +0700

    TCP multi chat client code

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ClientClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.list_chats = new System.Windows.Forms.ListBox();
+            this.txt_msg = new System.Windows.Forms.TextBox();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // list_chats
+            // 
+            this.list_chats.FormattingEnabled = true;
+            this.list_chats.ItemHeight = 16;
+            this.list_chats.Location = new System.Drawing.Point(0, 0);
+            this.list_chats.Name = "list_chats";
+            this.list_chats.Size = new System.Drawing.Size(799, 388);
+            this.list_chats.TabIndex = 0;
+            // 
+            // txt_msg
+            // 
+            this.txt_msg.Location = new System.Drawing.Point(0, 394);
+            this.txt_msg.Multiline = true;
+            this.txt_msg.Name = "txt_msg";
+            this.txt_msg.Size = new System.Drawing.Size(660, 50);
+            this.txt_msg.TabIndex = 1;
+            // 
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(666, 394);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(133, 50);
+            this.btn_send.TabIndex = 2;
+            this.btn_send.Text = "send";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_send);
+            this.Controls.Add(this.txt_msg);
+            this.Controls.Add(this.list_chats);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox list_chats;
+        private System.Windows.Forms.TextBox txt_msg;
+        private System.Windows.Forms.Button btn_send;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace ClientClient
+{
+    public partial class Form1 : Form
+    {
+
+        public TcpClient m_client;
+        public NetworkStream m_stream;
+        public BinaryReader m_reader;
+        public BinaryWriter m_writer;
+
+        public Thread m_thread;
+
+        public delegate void dlgFunc(string text);
+
+        public void invokeFn(string text)
+        {
+            this.Invoke(new dlgFunc(updateChat), new object[] { text });
+        }
+        public void updateChat(string text)
+        {
+            this.list_chats.Items.Add(text);
+        }
+        public void listenServer()
+        {
+            try
+            {
+                string text;
+                while ((text = m_reader.ReadString()) != string.Empty)
+                {
+                    invokeFn(text);
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+        public void start()
+        {
+            m_client = new TcpClient();
+            m_client.Connect("127.0.0.1", 3000);
+            m_stream = m_client.GetStream();
+            m_reader = new BinaryReader(m_stream, Encoding.UTF8);
+            m_writer = new BinaryWriter(m_stream, Encoding.UTF8);
+
+            m_thread = new Thread(listenServer);
+            m_thread.Start();
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            string text = this.txt_msg.Text;
+            if(text != string.Empty)
+            {
+                m_writer.Write(text);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.start();
+        }
+    }
+}
